3ca93773bb57b75cb8760a0fb06a55dd671cef96
germbault451 <71145763+germbault451@users.noreply.github.com>
2021-03-22 02:24:59 -0400

    Add files via upload

---
+++ precache-manifest.b487be036fde5f163c8bd72963cb7ce4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f490a935a7307fefce51fe5a74e5381",
+    "url": "/index.html"
+  },
+  {
+    "revision": "315ab0fe378286f12376",
+    "url": "/static/css/main.144964ab.chunk.css"
+  },
+  {
+    "revision": "0063a43e4902464c534e",
+    "url": "/static/js/2.a95cf96c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.a95cf96c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315ab0fe378286f12376",
+    "url": "/static/js/main.73e809af.chunk.js"
+  },
+  {
+    "revision": "5019ece4a74a1657600b",
+    "url": "/static/js/runtime-main.139310ea.js"
+  },
+  {
+    "revision": "6f82017092b3f62c335565b373a29482",
+    "url": "/static/media/KronaOne-Regular.6f820170.ttf"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
